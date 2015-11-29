bcbd0a3a5e0d54b50a2a9fc436c29376cf53edde
zklouz <zied.klouz@gmail.com>
2015-11-29 01:16:23 +0100

    Make a separate folder for libraries

---
+++ lib/misc.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+
+
+# Initiation of the session data
+function MISC_initialize_param ()
+{
+	verb=$1
+	dut1=$2
+	locat=$3
+	time=$4
+}
+
+# Special initiation of the session data
+function MISC_initialize_param_extra ()
+{
+	verb=$1
+	dut1=$2
+	locat=$3
+	time=$4
+	param=$5
+}
+
